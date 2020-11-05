1066eb5859abf749a7343a06d005009e206b5d3d
Tamir <tamirshina@gmail.com>
2020-11-05 11:49:26 +0200

    cool commit

--- FirmusEx/Class1.cs
+++ FirmusEx/Class1.cs
@@ -99,7 +99,6 @@ namespace FirmusEx
 						linkedItersectingElms.Add(intsecEl.Name);
 					}
 				}
-				//git ddd
 				ResultClass resClass = new ResultClass(Int32.Parse(el.Id.ToString()),
 					el.Document.Title, interSectsInLinked.Any(), linkedItersectingElms);
 
